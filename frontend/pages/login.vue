1020c87aa0d0c76c27f0c99fcb92ce7185d79ef5
chen <cycdanielboi@gmail.com>
2023-06-22 22:16:49 +0900

    create login /signup static page

---
+++ frontend/pages/login.vue
@@ -0,0 +1,10 @@
+<template>
+  <main class="container mx-auto px-6 py-10">
+    <h1 class="mb-6 text-center text-4xl">Welcome Back</h1>
+    <Form actionLabel="Log In" />
+    <p class="mt-8 text-center">
+      New to here?&nbsp;
+      <nuxt-link to="/signup" class="text-accent">Create an account!</nuxt-link>
+    </p>
+  </main>
+</template>
---
+++ frontend/pages/signup.vue
@@ -0,0 +1,10 @@
+<template>
+  <main class="container mx-auto px-6 py-10">
+    <h1 class="mb-6 text-center text-4xl">Welcome To Kanban</h1>
+    <Form actionLabel="Sign Up" />
+    <p class="mt-8 text-center">
+      Already have an account?&nbsp;
+      <nuxt-link to="/login" class="text-accent">Please Login!</nuxt-link>
+    </p>
+  </main>
+</template>
